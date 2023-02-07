49c4add08fb1d33f40548fe1a5d78ca61a681fee
David_Motari <davemotari22@gmail.com>
2023-02-07 17:36:59 +0300

    Implementing routes that require authorization

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,6 +15,16 @@ class Auth:
         """
         returns false, and true if auth is required
         """
+
+        if path is None or excluded_paths is None:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
