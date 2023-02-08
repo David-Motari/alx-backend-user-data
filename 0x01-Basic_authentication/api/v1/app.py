2f823c00c61e170ebd43e56f958f410098ef42fc
David_Motari <davemotari22@gmail.com>
2023-02-08 11:37:42 +0300

    Initial implementation of basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,13 +14,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth = getenv('AUTH_TYPE')
+AUTH_TYPE = getenv('AUTH_TYPE')
 
-if auth == "basic_auth":
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-elif auth == "auth":
+elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
