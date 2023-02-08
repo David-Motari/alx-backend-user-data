71c9a9ca2a61cc6f733abb5bbb8fe46694c3e723
David_Motari <davemotari22@gmail.com>
2023-02-08 12:05:02 +0300

    extration of header implementation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,18 @@ class BasicAuth(Auth):
     """
     inherits from auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extracting auth header
+        """
+        if (
+            authorization_header is None
+            or isinstance(authorization_header, str) is False
+            or not authorization_header.startswith("Basic ")
+            and not authorization_header.endswith(" ")
+        ):
+            return None
+
+        return authorization_header.split(" ")[1]
