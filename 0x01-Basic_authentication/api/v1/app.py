cd1e518149f9c3f6262a88fced1979e4f95836f7
David_Motari <davemotari22@gmail.com>
2023-02-08 11:29:31 +0300

    Initial implementation of basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,10 @@ auth = None
 
 auth = getenv('AUTH_TYPE')
 
-if auth:
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+basic_auth
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    inherits from auth
+    """
+    pass
