bfc1a1d2f9e97b25f454b0271bddfac9f7327b18
David_Motari <davemotari22@gmail.com>
2023-02-10 15:05:41 +0300

    logout implementation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -42,3 +42,18 @@ class SessionAuth(Auth):
             i_d = self.user_id_for_session_id(session_cookie)
 
         return User.get(i_d)
+
+    def destroy_session(self, request=None):
+        """
+        deletes the user session / logout
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -36,3 +36,18 @@ def login() -> str:
             return u_json
         else:
             return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route("/auth_session/logout",
+                 methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """
+    endpoit for logging-out
+    """
+    from api.v1.app import auth
+
+    des_session = auth.destroy_session(request)
+    if des_session is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
