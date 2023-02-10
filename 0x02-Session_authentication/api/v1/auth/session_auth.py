9790fa9351defb0f734d03702bb25ae46e3eba1b
David_Motari <davemotari22@gmail.com>
2023-02-10 05:52:46 +0300

    Initial implementation of session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+session_auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session authentication class, inherits from auth
+    """
+    pass
