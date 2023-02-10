7e2be81219e235a04a39e6363ef11f098f246f28
David_Motari <davemotari22@gmail.com>
2023-02-10 12:43:43 +0300

    Creating cookie value from request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,12 +4,15 @@ auth
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
     """
     class for authentiation
     """
+
+    SESSION_NAME = getenv("SESSION_NAME")
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
@@ -23,22 +26,22 @@ class Auth:
         if l_path == 0:
             return True
 
-        slash_path = True if path[l_path - 1] == '/' else False
+        slash_path = True if path[l_path - 1] == "/" else False
 
         tmp_path = path
         if not slash_path:
-            tmp_path += '/'
+            tmp_path += "/"
 
         for exc in excluded_paths:
             l_exc = len(exc)
             if l_exc == 0:
                 continue
 
-            if exc[l_exc - 1] != '*':
+            if exc[l_exc - 1] != "*":
                 if tmp_path == exc:
                     return False
             else:
-                if exc[:-1] == path[:l_exc - 1]:
+                if exc[:-1] == path[: l_exc - 1]:
                     return False
 
         return True
@@ -49,10 +52,18 @@ class Auth:
         """
         if request is None:
             return None
-        return request.headers.get('Authorization', None)
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar("User"):
         """
         returns None
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        return request.cookies.get(self.SESSION_NAME)
