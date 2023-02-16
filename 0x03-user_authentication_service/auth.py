d2e9ba922e827cf93d1eeaa4cd80cda375013e31
David_Motari <davemotari22@gmail.com>
2023-02-16 15:09:53 +0300

    Creating session id method for auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,6 +40,20 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        session id creator
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """
