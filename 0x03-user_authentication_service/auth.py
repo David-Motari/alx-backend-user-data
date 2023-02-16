e7ed2d306e8dbaf21ca2dadd67d84b2f374838e9
David_Motari <davemotari22@gmail.com>
2023-02-16 16:01:06 +0300

    Implementation of get user by session id in auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,6 +54,17 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        finding user wit session_id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user:
+                return user
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """
