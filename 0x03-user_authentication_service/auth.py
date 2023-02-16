09f592cbe3fbed0a3e85eae6398dd1ba1c9ef52a
David_Motari <davemotari22@gmail.com>
2023-02-16 13:17:13 +0300

    Register user methon in auth file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,29 @@
 auth
 """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registers user and returns the user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=pwd)
+            return user
+        else:
+            raise ValueError("User {} already exists".format(email))
 
 
 def _hash_password(password: str) -> bytes:
