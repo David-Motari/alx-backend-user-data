4b771d916fe6ad18c5158f215ea837e247b28a54
David_Motari <davemotari22@gmail.com>
2023-02-17 16:02:37 +0300

    endpoint for resetting user password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,8 +89,7 @@ def get_reset_password_token():
     email = request.form['email']
     if email:
         token = AUTH.get_reset_password_token(email)
-        if token:
-            return jsonify({"email": email, "reset_token": token}), 200
+        return jsonify({"email": email, "reset_token": token}), 200
     abort(403)
 
 
