bd78ffb614cc5a019ed30ef46d440cd3f51b6a00
David_Motari <davemotari22@gmail.com>
2023-02-17 17:03:30 +0300

    Endpoint for updating password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,12 +59,12 @@ def logout():
     """
     logout endpoint that destroys the session id
     """
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get("session_id")
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            return redirect('/')
+            return redirect("/")
     abort(403)
 
 
@@ -73,7 +73,7 @@ def profile():
     """
     gets user profile based on session_id
     """
-    session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get("session_id")
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
@@ -87,12 +87,31 @@ def get_reset_password_token():
     endpoint for resetting user password
     """
     try:
-        email = request.form['email']
-        if email:
-            token = AUTH.get_reset_password_token(email)
-            return jsonify({"email": email, "reset_token": token}), 200
+        email = request.form["email"]
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """
+    endpoint for updating password
+    """
+    try:
+        email = request.form["email"]
+        reset_token = request.form["reset_token"]
+        new_password = request.form["new_password"]
+    except KeyError:
+        abort(400)
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
