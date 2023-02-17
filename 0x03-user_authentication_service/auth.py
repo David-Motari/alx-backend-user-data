6c1e2ed6dcd56d540a6222fc4df6250a7b12d140
David_Motari <davemotari22@gmail.com>
2023-02-17 15:41:05 +0300

    reset_password token in auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
             raise ValueError
 
         reset_token = _generate_uuid()
-        self._db.update_user(user.user_id, reset_token=reset_token)
+        self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
 
