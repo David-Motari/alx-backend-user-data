0e6e600f041357c7c69964c6c32fbda2294fa6c2
David_Motari <davemotari22@gmail.com>
2023-02-17 15:35:10 +0300

    reset_password token in auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,6 +78,18 @@ class Auth:
 
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        generates password reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoresultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.user_id, reset_token=reset_token)
+
 
 def _hash_password(password: str) -> bytes:
     """
